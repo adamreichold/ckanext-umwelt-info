7adf7388dd9b2e8c8ad432258bb5bedb400072d1
Adam Reichold <adam.reichold@t-online.de>
2022-08-05 08:59:39 +0200

    Start implementing the Wasser-DE harvester.

--- ckanext/umwelt_info/harvesters/wasser_de.py
+++ ckanext/umwelt_info/harvesters/wasser_de.py
@@ -1,7 +1,14 @@
 import logging
+import json
 
+import requests
+
+from ckan.plugins import toolkit
 from ckan.plugins.core import SingletonPlugin, implements
 from ckanext.harvest.interfaces import IHarvester
+
+
+log = logging.getLogger(__name__)
 
 
 class WasserDEHarvester(SingletonPlugin):
@@ -15,10 +22,72 @@ class WasserDEHarvester(SingletonPlugin):
         }
 
     def gather_stage(self, harvest_job):
-        return []
+        url = (
+            harvest_job.source.url
+            + "rest/BaseController/FilterElements/V_REP_BASE_VALID"
+        )
+        log.debug("Gathering documents from %s", url)
+
+        response = requests.post(url, json={"filter": {}})
+        response.raise_for_status()
+        response = response.json()
+
+        count = len(response["V_REP_BASE_VALID"])
+        log.debug("Retrieved %d documents from %s", count, url)
+
+        # TODO: diff this against what is already in the database
+        ids = []
+
+        for document in response["V_REP_BASE_VALID"]:
+            if not document["NAME"]:
+                continue
+
+            id = document["ID"]
+            guid = f"wasser-de-{id}"
+
+            harvest_object = HarvestObject(
+                guid=guid,
+                job=harvest_job,
+            )
+
+            harvest_object.content = json.dumps(document)
+
+            harvest_object.save()
+            ids.append(harvest_object.id)
+
+        return ids
 
     def fetch_stage(self, harvest_object):
-        return "unchanged"
+        # Nothing to do since the document is already stored in the harvest object
+        return True
 
     def import_stage(self, harvest_object):
+        document = json.loads(harvest_object.content)
+
+        id = document["ID"]
+        title = document["NAME"]
+        description = document["TEASERTEXT"] or document["AUTOTEASERTEXT"] or ""
+
+        log.debug("Importing document %d", id)
+
+        context = {
+            "model": model,
+            "session": model.Session,
+            "ignore_auth": True,
+            "defer_commit": True,  # See ckan/ckan#1714
+        }
+
+        # TODO: get the correct user for the harvest object
+        site_user = toolkit.get_action("get_site_user")(context, {})
+
+        context = {
+            "model": model,
+            "session": model.Session,
+            "user": site_user["name"],
+            "ignore_auth": True,
+        }
+
+        # TODO: create package_dict
+        # TODO: create package
+
         return "unchanged"
